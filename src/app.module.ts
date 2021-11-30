f40151577739ccabd9e6e27ea8222cbfd5ea50f9
Rony Moura <ronymmoura@gmail.com>
2021-11-30 18:31:54 -0300

    Add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
+require('dotenv/config');
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+console.log(process.env.MONGODB_CONNECTION);
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
